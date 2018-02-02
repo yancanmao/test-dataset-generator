7d896261a8085d21b8079ec5d67000d40f1049b3
maoyancan <maoyancan@qq.com>
2018-02-02 02:08:50 -0800

    add file to stream

--- generator/src/main/java/fi/aalto/dmg/generator/FileToStream.java
+++ generator/src/main/java/fi/aalto/dmg/generator/FileToStream.java
@@ -1,8 +1,8 @@
 package fi.aalto.dmg.generator;
 
+import java.io.*;
 import fi.aalto.dmg.statistics.ThroughputLog;
 import fi.aalto.dmg.util.Constant;
-import fi.aalto.dmg.util.FastZipfGenerator;
 import fi.aalto.dmg.util.Utils;
 import org.apache.commons.math3.random.RandomDataGenerator;
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -36,24 +36,36 @@ public class FileToStream extends Generator {
     public void generate(int sleep_frequency) throws InterruptedException {
 
         long time = System.currentTimeMillis();
-
+        String sCurrentLine;
+        FileReader stream = null; 
         ThroughputLog throughput = new ThroughputLog(this.getClass().getSimpleName());
         // // for loop to generate message
-        throughput.execute();
         BufferedReader br = null;
-        stream = this.getClass().getClassLoader().getResourceAsStream("centroids.txt");
-        br = new BufferedReader(new InputStreamReader(stream));
+        int sent_sentences = 0;
+        try {
+        //stream = this.getClass().getClassLoader().getResourceAsStream("CJ20100913.txt");
+        stream = new FileReader("/root/CJ20100913.txt");
+        br = new BufferedReader(stream);
         while ((sCurrentLine = br.readLine()) != null) {
+            throughput.execute();
+            sent_sentences++;
             ProducerRecord<String, String> newRecord = new ProducerRecord<>(TOPIC, sCurrentLine);
             producer.send(newRecord);
-            // if (sleep_frequency > 0 && sent_sentences % sleep_frequency == 0) {
-            //     Thread.sleep(1);
-            // }
-            Thread.sleep(10000);
+             if (sleep_frequency > 0 && sent_sentences % sleep_frequency == 0) {
+                 Thread.sleep(1);
+             }
+            //Thread.sleep(1000);
         }
-        // control data generate speed
-        
-        // }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(stream != null) stream.close();
+                if(br != null) br.close();
+            } catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
         producer.close();
         logger.info("LatencyLog: " + String.valueOf(System.currentTimeMillis() - time));
     }
